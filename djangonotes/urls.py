d0fe8bf9e5dfab746306cc7b1845665a2731346a
oleshonchar <borovikoles@gmail.com>
2018-06-04 18:23:51 +0300

    Added base logic

--- djangonotes/urls.py
+++ djangonotes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test_page'),
+    path('', views.note_list, name='note_list'),
+    path('new/', views.note_new, name='note_new'),
 ]
--- djangonotes/views.py
+++ djangonotes/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Note
+from .forms import NoteForm
+from django.shortcuts import redirect
 
 
-def test(request):
-    return render(request, 'notes/post_add.html')
+def note_list(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/post_show.html', {'notes': notes})
+
+
+def note_new(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('note_list')
+    else:
+        form = NoteForm()
+    return render(request, 'notes/post_add.html', {'form': form})
